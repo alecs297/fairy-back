4ad54e88827ebf75752aa993d47f98ea4894fdb3
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2023-03-19 14:41:01 +0100

    get / update / create

--- api/v1.js
+++ api/v1.js
@@ -3,52 +3,90 @@ import { Router } from "express";
 import dotenv from "dotenv";
 
 import jwt from "../utils/jwt.js";
-import { login } from "../utils/auth.js";
+import { login, register } from "../utils/auth.js";
 import Split from "../models/split.js";
 
 dotenv.config();
-const token = new jwt(process.env.JWT_CERT, process.env.JWT_KEY);
+const tokenGenerator = new jwt(process.env.JWT_CERT, process.env.JWT_KEY, process.env.JWT_DURATION);
 
-export default Router()
-    .post("/login", async (req, res) => {
-        const { username, password } = req.body;
+const router =  Router()
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        let user = await login(username, password)
+        const token = tokenGenerator.sign({
+            username: user.username,
+            name: user.name
+        });
+        res.json({ token });
+    } catch (error) {
+        res.json({status: 401, message: error.message });
+    }
+})
+
+router.post("/register", async (req, res) => {
+    const { username, password, name } = req.body;
+    try {
+        let user = await register(username, password, name);
+        const token = tokenGenerator.sign({
+            username: user.username,
+            name: user.name
+        });
+        res.setHeader("Authorization", token);
+    } catch (error) {
+        res.json({status: 401, message: error.message });
+    }
+})
+
+router.get("/splits/:id", async (req, res) => {
+    const split = await Split.findById(req.params.id);
+    res.json(split);
+})
+
+router.put("/splits/:id", tokenGenerator.validateToken, async (req, res) => {
+    const split = await Split.findOne({
+        _id: req.params.id,
+        users: req.user.username
+    });
+    if (split) {
+        let hasUniqueMembers = req.body.users.reduce((acc, member) => {
+            if (!member || acc.map(e => e?.trim?.()).includes(member.trim())) {
+                return false;
+            }
+            return [...acc, member];
+        }, []).length === req.body.users.length;
+
+        if (!hasUniqueMembers) throw new Error("Members must be unique");
+
+        let splitsHaveMembers = req.body.transactions.filter(transaction => !req.body.users.includes(transaction.payer)).length === 0;
+
+        if (!splitsHaveMembers) throw new Error("All transactions must have a payer that is a member of the split");
+
+        split.name = req.body.name;
+        split.users = req.body.users;
+        split.transactions = req.body.transactions;
+
         try {
-            let user = login(username, password)
-            const token = token.sign({
-                username: user.username,
-                name: user.name
-            });
-            res.json({ token });
+            await split.save();
+            res.json(split);
         } catch (error) {
-            res.status(401).json({ message: error.message });
+            res.json({status: 500, message: "Internal server error"});
         }
-    })
-    .post("/register", async (req, res) => {
-        const { username, password, name } = req.body;
-        try {
-            let user = register(username, password, name);
-            const token = token.sign({
-                username: user.username,
-                name: user.name
-            });
-            res.json({ token });
-        } catch (error) {
-            res.status(401).json({ message: error.message });
-        }
-    })
-    .get("/splits/:id", async (req, res) => {
-        const split = await Split.findById(req.params.id);
-        res.json(split);
-    })
-    .get("/splits", token.validateToken, async (req, res) => {
-        const splits = await Split.find({ users: req.user.username });
-        res.json(splits);
-    })
-    .post("/splits", token.validateToken, async (req, res) => {
-        const split = new Split();
-        split.name = "New Split";
-        split.date = new Date();
-        split.users = [req.user.username];
-        await split.save();
-        res.json(split);
-    })
+    }
+})
+
+router.get("/splits", tokenGenerator.validateToken, async (req, res) => {
+    const splits = await Split.find({ users: req.user.username });
+    res.json(splits);
+})
+
+router.post("/splits", tokenGenerator.validateToken, async (req, res) => {
+    const split = new Split();
+    split.name = "New Split";
+    split.date = new Date();
+    split.users = [req.user.username];
+    await split.save();
+    res.json(split);
+})
+
+export default router;
